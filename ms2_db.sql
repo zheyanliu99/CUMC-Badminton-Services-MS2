3d3eb797b0eb8a74af5007664a679c8e2796e6cf
“Zheyan <zheyan.liu@columbia.edu>
2022-10-14 02:43:05 -0400

    update session and waitlist table

--- ms2_db.sql
+++ ms2_db.sql
@@ -1,6 +1,6 @@
 use ms2_db;
 
-
+-- Create table
 DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
@@ -14,6 +14,25 @@ CREATE TABLE users
     credits	int default 100
 );
 
+DROP TABLE IF EXISTS sessions;
+CREATE TABLE sessions
+(
+    sessionid int auto_increment primary key,
+    begintime DATETIME not null,
+    endtime DATETIME not null,
+    capacity int default 8
+);
+
+DROP TABLE IF EXISTS waitlist;
+CREATE TABLE waitlist
+(
+    sessionid int references sessions.sessionid,
+    userid int references users.userid,
+    updatetime DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    primary key (sessionid, userid)
+);
+
+-- Insertion
 INSERT INTO users (email, password, username, sex, birthday, credits ) VALUES ('test5@test.com', '123456', 'panda', 'female', '2014-01-01', '90');
 INSERT INTO users (email, password, username, sex, birthday, credits ) VALUES ('test4@test.com', '123456', 'zebra', 'female', '2014-01-01', '60');
 INSERT INTO users (email, password, username, sex, birthday, credits ) VALUES ('test3@test.com', '123456', 'orange', 'female', '2015-01-01', '0');
@@ -22,5 +41,17 @@ INSERT INTO users (email, password, username, sex, birthday, credits ) VALUES ('
 INSERT INTO users (email, password, username, sex, birthday, credits ) VALUES ('test@test.com', '123456', 'mushroom', 'male', '2021-01-01', '100');
 INSERT INTO users (email, password, username, sex, birthday) VALUES ('test6@test.com', '123456', 'panda', 'female', '2010-01-01');
 
+INSERT INTO sessions (begintime, endtime) VALUES ('2022-10-17 18:30:00', '2022-10-17 19:30:00');
+INSERT INTO sessions (begintime, endtime) VALUES ('2022-10-18 18:30:00', '2022-10-18 19:30:00');
+INSERT INTO sessions (begintime, endtime) VALUES ('2022-10-19 18:30:00', '2022-10-19 19:30:00');
+INSERT INTO sessions (begintime, endtime) VALUES ('2022-10-20 18:30:00', '2022-10-20 19:30:00');
+
+INSERT INTO waitlist (sessionid, userid) VALUES (1,1);
+INSERT INTO waitlist (sessionid, userid) VALUES (1,3);
+INSERT INTO waitlist (sessionid, userid) VALUES (1,6);
+
+
 SELECT * FROM users;
+SELECT * FROM sessions;
+SELECT * FROM waitlist;
 
